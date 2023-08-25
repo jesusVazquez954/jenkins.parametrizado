d4e4f3f69449f1b9558332b1605cccb5e3e28fba
Texwell <40063726+jesusVazquez954@users.noreply.github.com>
2023-08-25 16:06:18 -0600

    Create nodeJsDSL.groovy

---
+++ nodeJsDSL.groovy
@@ -0,0 +1,23 @@
+job('nodejs application DSL') {
+    description('Creating an application using node.js and jenkins with help of groovy')
+
+    scm {
+        git('https://github.com/macloujulian/nodejsapp.git', 'master') { node -> 
+            node / gitConfigName('Elliot')
+            node / gitCongifEmail('elliot@fsociety.com')
+        }
+    }
+
+    triggers {
+        scm('H/7 * * * *')
+    }
+
+    wrappers {
+        nodejs('NodeJs-14.11.0')
+    }
+
+    steps {
+        shell('echo "========EXECUTING PROCESS========"')
+        shell('npm install')
+    }
+}
