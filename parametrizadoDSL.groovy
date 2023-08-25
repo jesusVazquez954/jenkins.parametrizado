527081513b8c5c0123aaf0e81d4a800a8db88341
jesusVazquez954 <9jesusmv9@gmail.com>
2023-08-25 10:39:45 -0600

    First commit

---
+++ parametrizadoDSL.groovy
@@ -0,0 +1,31 @@
+job('job-using-dsl') {
+  
+	description('Creating a job using a seed job D:')
+	scm {
+	git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
+	node / gitConfigName('Elliot')
+		node / gitConfigEmail('elliot@fsociety.com')
+		}
+	}
+ 
+    parameters {
+	stringParam('nombre', defaultValue = 'Elliot', description = "Name of the person executing the job")
+	choiceParam('Skills', ['Hacking', 'Programmer', 'Tester'])
+	booleanParam('Beginner', false)
+    }
+  
+  	triggers {
+  		cron('H/7 * * * *') 
+    }
+  
+    steps {
+    	shell("bash jobscript.sh")
+    }
+  
+    publishers {
+    	mailer('correoparavirus459@gmail.com', true, true);
+    }
+  
+ 
+  
+}
